90f1a139b193ebc61826fcf32f828045eb1b0a33
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 22:05:12 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,11 @@
 class Project
   
   attr_reader :title 
-  attr_accessor :backed_projects
+  attr_accessor :backers
   
   def initialize(title)
     @title = title 
-    @backed_projects = []
+    @backers = []
   end 
   
     
